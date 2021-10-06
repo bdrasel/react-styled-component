c7d78afcb20b0b2e18575c17a894809bfebfddd0
Web Developer <55748402+bdrasel@users.noreply.github.com>
2021-10-06 15:11:27 +0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import { Button } from "./styles/Button.styled";
 import { Container } from "./styles/Container.styled";
 import { Flex } from "./styles/Flex.styled";
-import { Nav, StyleHeader Image } from "./styles/Header.styled";
+import { Nav, StyleHeader, Image } from "./styles/Header.styled";
 
 export default function Header() {
     return (
